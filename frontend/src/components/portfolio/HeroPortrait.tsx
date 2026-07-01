2d3efd57bee1962435cb5ca8faec0db3e889e99c
Mantle Bearer <igbokwegoodluck8@gmail.com>
2026-07-01 09:14:04 +0100

    add HeroPortrait Components

--- frontend/src/components/portfolio/HeroPortrait.tsx
+++ frontend/src/components/portfolio/HeroPortrait.tsx
@@ -1,10 +1,8 @@
 import { portfolioAssets, portfolioProfile } from "@/data/portfolio";
-import { AccentRings } from "@/components/portfolio/AccentRings";
 
 export function HeroPortrait() {
   return (
     <figure className="hero-portrait">
-      <AccentRings />
       <img src={portfolioAssets.heroPortrait.src} alt={`${portfolioProfile.name}, ${portfolioProfile.role}`} />
     </figure>
   );
